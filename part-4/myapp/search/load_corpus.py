29d1a00557e4c40550e36ad6a0efd2efbf15f159
Sara <sara.soriano02@estudiant.upf.edu>
2023-11-16 18:10:07 +0100

    Update load_corpus.py

--- part-4/myapp/search/load_corpus.py
+++ part-4/myapp/search/load_corpus.py
@@ -3,6 +3,9 @@ import pandas as pd
 from myapp.core.utils import load_json_file
 from myapp.search.objects import Document
 
+from pandas import json_normalize
+import json
+
 _corpus = {}
 
 
@@ -23,95 +26,36 @@ def _load_corpus_as_dataframe(path):
     Load documents corpus from file in 'path'
     :return:
     """
-    json_data = load_json_file(path)
-    tweets_df = _load_tweets_as_dataframe(json_data)
-    _clean_hashtags_and_urls(tweets_df)
-    # Rename columns to obtain: Tweet | Username | Date | Hashtags | Likes | Retweets | Url | Language
-    corpus = tweets_df.rename(
-        columns={"id": "Id", "full_text": "Tweet", "screen_name": "Username", "created_at": "Date",
-                 "favorite_count": "Likes",
-                 "retweet_count": "Retweets", "lang": "Language"})
+    data_list = []
 
-    # select only interesting columns
-    filter_columns = ["Id", "Tweet", "Username", "Date", "Hashtags", "Likes", "Retweets", "Url", "Language"]
-    corpus = corpus[filter_columns]
-    return corpus
+    #read all lines
+    with open(path) as f:
+        for line in f:
+            # Parse the string into a JSON object
+            json_data = json.loads(line)
+            # Append the JSON object to the list
+            data_list.append(json_data)
 
+    # Convert the list of JSON objects to a DataFrame
+    df = pd.DataFrame(data_list)
+    
+    for index, row in df.iterrows():
+        screen_name = row['user']['screen_name'] # Access the 'screen_name' key
 
-def _load_tweets_as_dataframe(json_data):
-    data = pd.DataFrame(json_data).transpose()
-    # parse entities as new columns
-    data = pd.concat([data.drop(['entities'], axis=1), data['entities'].apply(pd.Series)], axis=1)
-    # parse user data as new columns and rename some columns to prevent duplicate column names
-    data = pd.concat([data.drop(['user'], axis=1), data['user'].apply(pd.Series).rename(
-        columns={"created_at": "user_created_at", "id": "user_id", "id_str": "user_id_str", "lang": "user_lang"})],
-                     axis=1)
-    return data
+        # Create the 'Url' and 'Hashtags' column for each row
+        df.at[index, "Url"] = f"https://twitter.com/{screen_name}/status/{df['id_str'].iloc[index]}"
+        df.at[index, "Hashtags"] = len(row['entities']['hashtags'])
 
-
-def _build_tags(row):
-    tags = []
-    # for ht in row["hashtags"]:
-    #     tags.append(ht["text"])
-    for ht in row:
-        tags.append(ht["text"])
-    return tags
-
-
-def _build_url(row):
-    url = ""
-    try:
-        url = row["entities"]["url"]["urls"][0]["url"]  # tweet URL
-    except:
-        try:
-            url = row["retweeted_status"]["extended_tweet"]["entities"]["media"][0]["url"]  # Retweeted
-        except:
-            url = ""
-    return url
-
-
-def _clean_hashtags_and_urls(df):
-    df["Hashtags"] = df["hashtags"].apply(_build_tags)
-    df["Url"] = df.apply(lambda row: _build_url(row), axis=1)
-    # df["Url"] = "TODO: get url from json"
-    df.drop(columns=["entities"], axis=1, inplace=True)
-
-
-def load_tweets_as_dataframe2(json_data):
-    """Load json into a dataframe
-
-    Parameters:
-    path (string): the file path
-
-    Returns:
-    DataFrame: a Panda DataFrame containing the tweet content in columns
-    """
-    # Load the JSON as a Dictionary
-    tweets_dictionary = json_data.items()
-    # Load the Dictionary into a DataFrame.
-    dataframe = pd.DataFrame(tweets_dictionary)
-    # remove first column that just has indices as strings: '0', '1', etc.
-    dataframe.drop(dataframe.columns[0], axis=1, inplace=True)
-    return dataframe
-
-
-def load_tweets_as_dataframe3(json_data):
-    """Load json data into a dataframe
-
-    Parameters:
-    json_data (string): the json object
-
-    Returns:
-    DataFrame: a Panda DataFrame containing the tweet content in columns
-    """
-
-    # Load the JSON object into a DataFrame.
-    dataframe = pd.DataFrame(json_data).transpose()
-
-    # select only interesting columns
-    filter_columns = ["id", "full_text", "created_at", "entities", "retweet_count", "favorite_count", "lang"]
-    dataframe = dataframe[filter_columns]
-    return dataframe
+    df = df.rename(columns={
+        "id": "Id",
+        "full_text": "Tweet",
+        "screen_name": "Username",
+        "created_at": "Date",
+        "favorite_count": "Likes",
+        "retweet_count": "Retweets",
+        "lang": "Language"
+    })
+    return df
 
 
 def _row_to_doc_dict(row: pd.Series):
